042f69d91c47e53ceb7422ce496cf023a816c9be
c4fiber <qudcjf153@gmail.com>
2024-01-15 20:40:30 +0900

    chore: init spring boot project

---
+++ backend/src/test/java/com/magic/bobbuddy/BobbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magic.bobbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
